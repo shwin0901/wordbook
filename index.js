22cc94eecb28f93b9500e4682176c535e4cd6196
shwin0901 <401815708@qq.com>
2019-09-03 14:24:13 +0800

    实现含义按钮功能

--- index.js
+++ index.js
@@ -15,9 +15,19 @@ const meaningDiv = `
                 <option>adv.</option>
             </select>
             <input class="meaning-text" type="text" placeholder="中文意思" required>
-            <div class="btn btn-primary"></div>
+            <div class="btn btn-primary" onclick="onMeaningButtonClick(event)"></div>
      </div>
 `;
 
 let sectionMeaning = document.querySelector('.section-meaning');
 sectionMeaning.insertAdjacentHTML('beforeend', meaningDiv);
+
+function onMeaningButtonClick(event) {
+    let target = event.target;
+    let sign = window.getComputedStyle(target,':before').getPropertyValue('content');
+    if (sign.includes('+')) {
+        sectionMeaning.insertAdjacentHTML('beforeend', meaningDiv);
+    } else {
+        target.parentNode.remove();
+    }
+}
