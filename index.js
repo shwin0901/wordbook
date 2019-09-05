bb98c17cf367c577b2f1866b6930794da72e11ab
shwin0901 <401815708@qq.com>
2019-09-05 20:17:40 +0800

    检查用户输入的信息是否合法

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const meaningDiv = `
                 <option>adj.</option>
                 <option>adv.</option>
             </select>
-            <input class="meaning-text" type="text" placeholder="中文意思" required>
+            <input class="meaning-text" type="text" placeholder="中文意思" required oninput="onInputCharge(event)">
             <div class="btn btn-primary" onclick="onMeaningButtonClick(event)"></div>
      </div>
 `;
@@ -31,3 +31,26 @@ function onMeaningButtonClick(event) {
         target.parentNode.remove();
     }
 }
+
+let save = document.getElementById('save');
+save.onclick = function(event) {
+    event.preventDefault();
+   if( !inspectInput()) return;
+};
+
+function inspectInput() {
+    for (let input of document.querySelectorAll('input')) {
+        if (input.value === '') {
+            input.style.borderColor = 'red';
+            return false;
+        }
+    }
+    return true;
+}
+
+function onInputCharge(event) {
+    if(event.data !== null){
+        event.target.style.borderColor = null;
+    }
+}
+
