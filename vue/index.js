0eafb18532c826d81b2a43ee288bc043f15b0f20
shwin0901 <401815708@qq.com>
2019-10-27 21:25:44 +0800

    添加/删除含义按钮功能

--- vue/index.js
+++ vue/index.js
@@ -1,21 +1,65 @@
-Vue.component("word-input",{
-    template:`<input type="text" :placeholder="placeholder">`,
-    props:["placeholder"],
+Vue.component("word-input", {
+    template: `<input type="text" :placeholder="placeholder" v-model="value" 
+                     @input="onInputChange">`,
+    props: ["placeholder", 'value'],
+    methods: {
+        onInputChange: function() {
+            this.$emit('input', this.value)
+        }
+    }
+});
+
+Vue.component("meaning-div", {
+    template: `<div class="meaning">
+            <select v-model="value.type">
+                <option value="v.">v.</option>
+                <option value="n.">n.</option>
+                <option value="adj.">adj.</option>
+                <option value="adv.">adv.</option>
+            </select>
+            <input class="meaning-text" type="text" placeholder="中文意思" v-model="value.text">
+            <div class="btn btn-primary" @click="onMeaningButtonClick($event)"></div>
+     </div>`,
+    props:['value','index'],
+    methods:{
+        onMeaningButtonClick:function(event) {
+            let sign = window.getComputedStyle(event.target,':before').getPropertyValue('content');
+            this.$emit('meaning-click',sign,this.index);
+        }
+    }
 });
 
 const vm = new Vue({
     el: "#app",
     data: {
-        newWord:null,
+        newWord: {
+            name: '',
+            chinese:[
+                {
+                    type: 'v.',
+                    text: '',
+                }]
+        },
+        wordBook:[],
     },
-    methods:{
-        defaultWord:function() {
+    methods: {
+        defaultWord: function() {
             return {
-                name:'',
-                chinese:{
-                    type:'v.',
-                    text:'',
+                name: '',
+                chinese: {
+                    type: 'n.',
+                    text: '',
                 }
+            }
+        },
+        onMeaningClick:function(event,index) {
+            if (event.includes("+")) {
+                this.newWord.chinese.push({
+                    type: 'v.',
+                    text: '',
+                })
+            } else {
+                this.newWord.chinese.splice(index,1);
             }
         }
     }
